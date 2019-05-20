f9754604bdfa17a7edc3a5f0d5479ae229c3a9a3
Nataly2315 <41804623+Nataly2315@users.noreply.github.com>
2019-05-20 18:49:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,139 @@
 const TelegramBot = require('node-telegram-bot-api');
-const request = require('request');
+const request= require ('request');
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '544858820:AAHEDHeEqRu15boYGdeBTmdwj_zX0hVMioY';
+const token = '736680208:AAFBgN3plB2Kilgk6OYgrtbYJFfB3FLUZ2U';
 
-// Create a bot that uses 'polling' to fetch new updates
+
 const bot = new TelegramBot(token, {polling: true});
 
-bot.onText(/\/curse/, (msg, match) => {
+const flag = {
+    'EUR': '🇪🇺',
+    'USD': '🇺🇸',
+    'RUR': '🇷🇺',
+    'UAH': '🇺🇦'
+};
 
-  const chatId = msg.chat.id;
 
-  bot.sendMessage(chatId, 'Выберите какая валюта вас интересует', {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: '€ - EUR',
-            callback_data: 'EUR'
-          }, {
-            text: '$ - USD',
-            callback_data: 'USD'
-          }, {
-            text: '₽ - RUR',
-            callback_data: 'RUR'
-          }, {
-            text: '₿ - BTC',
-            callback_data: 'BTC'
-          }
-        ]
-      ]
-    }
-  });
+
+bot.onText(/\/currencies/, (msg, match) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId, 'Какая валюта вас интересует?', {
+        reply_markup: {
+            inline_keyboard: [
+                [
+                    {
+                        text:"€ EUR",
+                        callback_data: 'EUR',
+                    },
+
+                    {
+                        text:"$ USD",
+                        callback_data: 'USD',
+                    },
+
+                    {
+                        text:"₽ RUB",
+                        callback_data: 'RUR',
+                    },
+
+                ]
+
+            ]
+        }
+    });
 });
 
-bot.on('callback_query', query => {
-  const id = query.message.chat.id;
+bot.on('callback_query', query=> {
+    const id = query.message.chat.id;
+    if (query.message.text == "Какая валюта вас интересует?") {
+        request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
+            const data = JSON.parse(body);
+            const result = data.filter(item => item.ccy === query.data)[0];
 
-  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
-    const data = JSON.parse(body);
-    const result = data.filter(item => item.ccy === query.data)[0];
-    const flag = {
-      'EUR': '🇪🇺',
-      'USD': '🇺🇸',
-      'RUR': '🇷🇺',
-      'UAH': '🇺🇦',
-      'BTC': '₿'
+            let md = `* ${flag[result.ccy]} ${result.ccy} ➡ ${result.base_ccy}  ${flag[result.base_ccy]}* \nBuy: _${Math.trunc(result.buy * 100) / 100}_ \nSale: _${Math.trunc(result.sale * 100) / 100}_`;
+            bot.sendMessage(id, md, {parse_mode: 'Markdown'});
+
+        })
     }
-    let md = `
-      *${flag[result.ccy]} ${result.ccy} 💱 ${result.base_ccy} ${flag[result.base_ccy]}*
-      Buy: _${result.buy}_
-      Sale: _${result.sale}_
-    `;
-    bot.sendMessage(id, md, {parse_mode: 'Markdown'});
-  })
-})
+
+    else if (query.message.text == 'Выберите валюту для конвертации') {
+
+            request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
+
+                const cur= query.data.slice(0 ,3 );
+                var summ = +query.data.slice(3);
+                const data = JSON.parse(body);
+                const result = data.filter(item => item.ccy === cur)[0];
+                let res = (summ / result.sale);
+                let md = ` ${flag['UAH']} ${result.base_ccy} ➡ ${result.ccy}  ${flag[result.ccy]} \n*${summ}*  ${result.base_ccy} = *${Math.trunc(res * 100) / 100}* ${result.ccy}`;
+                bot.sendMessage(id, md, {parse_mode: 'Markdown'});
+
+            })
+    }
+});
+
+
+bot.onText(/\/converter/, (msg, match) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId,  'Введите сумму в гривнах которую необходимо конвертировать');
+
+
+
+});
+
+bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'Добрый день! Введите:\n\/currencies - что бы узнать курс валют\n\/converter - что бы конвертировать валюты');
+
+});
+
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    var summ = msg.text;
+
+
+    if (summ.match(/,/i)!= -1){summ = summ.replace(",",".");
+    console.log(summ)};
+    if ((summ!='/converter') && (summ!='/start') && (summ!='/currencies')){
+
+    summ = Number(summ);
+     if (isNaN(summ)){
+        bot.sendMessage(chatId,`Такой команды нет 😨 \n*Попробуйте ввести:*\n\/currencies - что бы узнать курс валют\n\/converter - что бы конвертировать валюты`, {parse_mode: 'Markdown'});
+    }
+
+    else {
+        bot.sendMessage(chatId,`Вы ввели *${summ}* грн`, {parse_mode: 'Markdown'});
+        bot.sendMessage(chatId,  'Выберите валюту для конвертации',
+            {
+                reply_markup: {
+                    inline_keyboard: [
+                        [
+                            {
+                                text: "€ EUR",
+                                callback_data: 'EUR'+summ,
+
+                            },
+
+                            {
+                                text: "$ USD",
+                                callback_data: 'USD'+summ,
+
+                            },
+
+                            {
+                                text: "₽ RUB",
+                                callback_data: 'RUR'+summ,
+
+                            },
+
+                        ]
+
+                    ]
+                }
+            });
+
+    }
+}});
